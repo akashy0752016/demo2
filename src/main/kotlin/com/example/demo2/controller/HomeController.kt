634b3197c0b4be88e810b07aa336271585e21886
akashy0752016 <akashy075@gmail.com>
2023-06-26 01:18:08 +0530

    Update HomeController.kt

--- src/main/kotlin/com/example/demo2/controller/HomeController.kt
+++ src/main/kotlin/com/example/demo2/controller/HomeController.kt
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController
 class HomeController {
     @GetMapping("/")
     fun home() : String {
-        return "Hello World"
+        return "Hello World Sonar Test"
     }
 }
